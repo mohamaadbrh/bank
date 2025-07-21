3e15608b1823e12f38fd184184ed260d76d7fa08
mohamaadbrh <homohamadmahdi@gmail.com>
2025-07-21 15:00:27 +0330

    4

--- borhani/adminwindow.cpp
+++ borhani/adminwindow.cpp
@@ -1,14 +1,55 @@
-#include "adminwindow.h"
-#include "ui_adminwindow.h"
+#include "AdminWindow.h"
+#include "AddCustomerDialog.h"
+#include "RemoveCustomerDialog.h"
+#include "ViewCustomersDialog.h"
+#include "ViewAccountsDialog.h"
+#include "EditProfileDialog.h"
+#include "LoginDialog.h"
+#include <QPushButton>
 
-AdminWindow::AdminWindow(QWidget *parent)
+AdminWindow::AdminWindow(Admin* admin,
+                         LinkedList<User>* users,
+                         QWidget* parent)
     : QMainWindow(parent)
-    , ui(new Ui::AdminWindow)
+    , m_admin(admin)
+    , m_userList(users)
 {
-    ui->setupUi(this);
+    ui.setupUi(this);
+    connect(ui.btnAddCustomer,      &QPushButton::clicked, this, &AdminWindow::on_btnAddCustomer_clicked);
+    connect(ui.btnRemoveCustomer,   &QPushButton::clicked, this, &AdminWindow::on_btnRemoveCustomer_clicked);
+    connect(ui.btnViewAllCustomers, &QPushButton::clicked, this, &AdminWindow::on_btnViewAllCustomers_clicked);
+    connect(ui.btnViewAllAccounts,  &QPushButton::clicked, this, &AdminWindow::on_btnViewAllAccounts_clicked);
+    connect(ui.btnEditProfile,      &QPushButton::clicked, this, &AdminWindow::on_btnEditProfile_clicked);
+    connect(ui.btnExit,             &QPushButton::clicked, this, &AdminWindow::on_btnExit_clicked);
 }
 
-AdminWindow::~AdminWindow()
-{
-    delete ui;
+void AdminWindow::on_btnAddCustomer_clicked() {
+    AddCustomerDialog dlg(m_admin, m_userList, this);
+    dlg.exec();
+}
+
+void AdminWindow::on_btnRemoveCustomer_clicked() {
+    RemoveCustomerDialog dlg(m_admin, m_userList, this);
+    dlg.exec();
+}
+
+void AdminWindow::on_btnViewAllCustomers_clicked() {
+    ViewCustomersDialog dlg(m_userList, this);
+    dlg.exec();
+}
+
+void AdminWindow::on_btnViewAllAccounts_clicked() {
+    ViewAccountsDialog dlg(m_userList, this);
+    dlg.exec();
+}
+
+void AdminWindow::on_btnEditProfile_clicked() {
+    EditProfileDialog dlg(m_admin, m_userList, this);
+    dlg.exec();
+}
+
+void AdminWindow::on_btnExit_clicked() {
+    this->close();
+    LoginDialog loginDlg;
+    loginDlg.exec();
 }
--- borhani/adminwindow.h
+++ borhani/adminwindow.h
@@ -1,22 +1,36 @@
+
 #ifndef ADMINWINDOW_H
 #define ADMINWINDOW_H
 
 #include <QMainWindow>
+#include "ui_AdminWindow.h"
+#include "LinkedList.h"
+#include "User.h"
+#include "Admin.h"
+#include "LoginDialog.h"
 
-namespace Ui {
-class AdminWindow;
-}
+using namespace std;
 
-class AdminWindow : public QMainWindow
-{
+class AdminWindow : public QMainWindow {
     Q_OBJECT
 
 public:
-    explicit AdminWindow(QWidget *parent = nullptr);
-    ~AdminWindow();
+    explicit AdminWindow(Admin* admin,
+                         LinkedList<User>* users,
+                         QWidget* parent = nullptr);
+
+private slots:
+    void on_btnAddCustomer_clicked();
+    void on_btnRemoveCustomer_clicked();
+    void on_btnViewAllCustomers_clicked();
+    void on_btnViewAllAccounts_clicked();
+    void on_btnEditProfile_clicked();
+    void on_btnExit_clicked();
 
 private:
-    Ui::AdminWindow *ui;
+    Ui::AdminWindow    ui;
+    Admin*             m_admin;
+    LinkedList<User>*  m_userList;
 };
 
 #endif // ADMINWINDOW_H
